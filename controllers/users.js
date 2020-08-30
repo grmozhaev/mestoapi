04fbe77573d74456c290cc323a4ae72af067e779
Grigory Mozhaev <grmozhaev@gmail.com>
2020-08-30 23:47:11 +0300

    Auth added
    Sign up + login added
    JWT auth added
    New user rights restrictions

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,8 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
+const secretKey = require('../utils/jwt_secret');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
@@ -21,17 +25,34 @@ module.exports.getUserById = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
 
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400).send({ message: err.message });
-      } else {
-        res.status(500).send({ message: err.message });
-      }
-    });
+  if (!password) {
+    res.status(400).send({ message: 'Пароль не может быть пустым' });
+    return;
+  }
+
+  bcrypt.hash(password, 10).then((hash) => {
+    User.create({
+      name, about, avatar, email, password: hash,
+    })
+      .then((user) => res.send({
+        _id: user._id,
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+      }))
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          res.status(400).send({ message: err.message });
+        } else {
+          res.status(500).send({ message: err.message });
+        }
+      });
+  });
 };
 
 module.exports.updateBio = (req, res) => {
@@ -79,5 +100,25 @@ module.exports.updateAvatar = (req, res) => {
       } else {
         res.status(500).send({ message: err.message });
       }
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, secretKey, { expiresIn: '7d' });
+
+      res
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+          sameSite: true,
+        })
+        .end();
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
     });
 };
